f0245a7e7f13c56effb009f5aac829eff2ff1895
Radu Crisan <radu.crisan@gmail.com>
2018-11-18 12:31:09 +0200

    update

--- app.go
+++ app.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"log"
+	"time"
 
+	"github.com/golang/protobuf/proto"
 	"github.com/mongodb/mongo-go-driver/bson/objectid"
 	"github.com/mongodb/mongo-go-driver/mongo"
 	"github.com/raducrisan1/microservice-persist/stockinfo"
@@ -24,11 +26,11 @@ type AnnotatedRating struct {
 }
 
 func main() {
-	//msgs, rabbitConn, rabbitChannel, err := createRabbitMqReader()
-	//failOnError(err, "Could not initialize rabbitmq reader")
-	//defer rabbitConn.Close()
-	//defer rabbitChannel.Close()
-	//defer rabbitChannel.Close()
+	msgs, rabbitConn, rabbitChannel, err := createRabbitMqReader()
+	failOnError(err, "Could not initialize rabbitmq reader")
+	defer rabbitConn.Close()
+	defer rabbitChannel.Close()
+	defer rabbitChannel.Close()
 
 	mng, err := mongo.NewClient("mongodb://localhost:27017")
 	failOnError(err, "Could not create a mongodb client")
@@ -36,26 +38,26 @@ func main() {
 
 	err = mng.Connect(context.Background())
 	failOnError(err, "Could not connect do mongodb server")
-	//coll := mng.Database("trading").Collection("ratings")
+	coll := mng.Database("trading").Collection("ratings")
 
 	stop := make(chan bool)
-	//limiter := time.Tick(time.Second / 70)
+	limiter := time.Tick(time.Second / 70)
 
 	//this goroutine reads messages from rabbitmq and writes them to mongodb
-	// go func() {
-	// 	for msg := range msgs {
-	// 		<-limiter
-	// 		//here, annotate the message with ObjectID and write it into mongodb
-	// 		protomsg := stockinfo.StockRating{}
-	// 		if err = proto.Unmarshal(msg.Body, &protomsg); err != nil {
-	// 			log.Printf("Could not deserialize protobuf message %v", msg.MessageId)
-	// 			continue
-	// 		}
-	// 		annotatedMsg := AnnotatedRating{StockRating: protomsg, ID: objectid.New()}
-	// 		coll.InsertOne(nil, annotatedMsg)
-	// 		msg.Ack(false)
-	// 	}
-	// }()
+	go func() {
+		for msg := range msgs {
+			<-limiter
+			//here, annotate the message with ObjectID and write it into mongodb
+			protomsg := stockinfo.StockRating{}
+			if err = proto.Unmarshal(msg.Body, &protomsg); err != nil {
+				log.Printf("Could not deserialize protobuf message %v", msg.MessageId)
+				continue
+			}
+			annotatedMsg := AnnotatedRating{StockRating: protomsg, ID: objectid.New()}
+			coll.InsertOne(nil, annotatedMsg)
+			msg.Ack(false)
+		}
+	}()
 
 	//this goroutine listens to gRPC calls, in our case from reports microservice
 	go func() {
